0e7522e7266464e9e3bc24cea7ec9ad36e2c401c
Victoria <144530721+vialvasileva@users.noreply.github.com>
2026-02-23 22:44:12 +0300

    Initial Commit

---
+++ TableApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableApp
+//
+//  Created by victoria on 23.02.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
